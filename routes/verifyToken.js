01fc5399823f68c5e7e9312ef0c4df1ed8c8f3fe
gabriosilva <gabriosilva123@gmail.com>
2022-04-03 23:13:59 -0300

    chore(auth): disable token authentication

--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -1,21 +1,26 @@
 const jwt = require("jsonwebtoken");
 
-module.exports = (req,res,next) =>{
-  try{
-    const authHeader = req.headers["authorization"];
-    const authType = authHeader && authHeader.split(" ")[0];
-    const token = authHeader && authHeader.split(" ")[1];
+module.exports = (req, res, next) => {
+  // try {
+  //   const authHeader = req.headers["authorization"];
+  //   const authType = authHeader && authHeader.split(" ")[0];
+  //   const token = authHeader && authHeader.split(" ")[1];
 
-    if (!token || authType != "Bearer" ) {
-      return res.status(500).send({error:"Invalid authorization!"});
-    }
+  //   if (!token || authType != "Bearer") {
+  //     return res.status(500).send({ error: "Invalid authorization!" });
+  //   }
 
-    const verified = jwt.verify(token,process.env.TOKEN_SECRET);
-    req.user = verified;
-    req.userId = req.user.id;
-    req.token = token;
-    next();
-  } catch(e) {
-    return res.status(401).send({error:"Invalid token!"})
-  }
-}
+  //   const verified = jwt.verify(token, process.env.TOKEN_SECRET);
+  //   req.user = verified;
+  //   req.userId = 1;
+  //   req.userCpf = "595.080.896-84"; //req.user.cpf;
+  //   req.token = token;
+  //   next();
+  // } catch (e) {
+  //   return res.status(401).send({ error: "Invalid token!" });
+  // }
+  
+  req.userId = 1;
+  req.userCpf = "595.080.896-84"; //req.user.cpf;
+  next();
+};
